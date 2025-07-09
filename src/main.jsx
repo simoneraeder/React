d83750da6615f54c2d2701735f2a3bde5cc75143
Simone Raeder Blasse Gorni <PET0099063@aluno.firjansenaisesi.com.br>
2025-07-09 18:44:23 -0300

    atualizado

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
-import { Router } from 'react-router'
+import { Router } from './routes/Router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>    
-      <App/>
+      <Router/>
     </BrowserRouter>  
   </StrictMode>,
 )
